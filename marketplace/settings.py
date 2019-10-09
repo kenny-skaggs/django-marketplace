54fb80b4d5b6c4132bc680eac7a0007137e828e1
skagzilla <skaggskd88@gmail.com>
2019-10-08 21:03:38 -0500

    getting a template set up

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'marketplace', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
