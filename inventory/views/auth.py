db90099fe824f0253b2c9bcb7d9ed148b72d0291
skagzilla <skaggskd88@gmail.com>
2019-10-14 20:39:29 -0500

    drying up some common auth view code

--- inventory/views/auth.py
+++ inventory/views/auth.py
@@ -5,18 +5,25 @@ from django.views.generic import RedirectView
 from django.views.generic.edit import FormView
 
 
-class LoginView(FormView):
+class AuthFormViewMixin:
+    def get_success_url(self):
+        return reverse('home')
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['hide_login'] = True
+        return context
+
+
+class LoginView(AuthFormViewMixin, FormView):
     template_name = 'login.html'
     form_class = AuthenticationForm
     
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super().form_valid(form)
-        
-    def get_success_url(self):
-        return reverse('home')
 
-class RegisterView(FormView):
+class RegisterView(AuthFormViewMixin, FormView):
     template_name = 'register.html'
     form_class = UserCreationForm
     
@@ -27,15 +34,6 @@ class RegisterView(FormView):
         user = authenticate(username=username, password=raw_password)
         login(self.request, user)
         return super().form_valid(form)
-    
-    def get_success_url(self):
-        return reverse('home')
-        
-    #TODO: make this a mixin
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['hide_login'] = True
-        return context
     
     
 class LogoutView(RedirectView):
